d9a3ea3514bd1a5c59319a6e3435c30f009807f5
TK <tp4thewin@gmail.com>
2026-02-18 13:09:29 -0500

    Client form now functional
    has base requirements, logger not included

--- ClientForm.java
+++ ClientForm.java
@@ -2,7 +2,39 @@ import javax.swing.*;
 
 public class ClientForm {
     private JPanel panel1;
+    private JLabel ClientID;
+    private JTextField clientIDTextField;
+    private JLabel ClientName;
+    private JTextField ClientNameTextField;
+    private JLabel jobDuration;
+    private JTextField jobDurationTextField;
+    private JLabel JobDeadline;
+    private JTextField jobDeadlineTextField;
+    private JButton backButton;
+    private JButton clearButton;
+    private JButton submitButton;
 
+
+    public ClientForm() {
+        backButton.addActionListener(e -> {
+            JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(panel1);
+            frame.setTitle("VCRTS - Select Role");
+            frame.setContentPane(new SelectRole().getPanel());
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+        });
+        clearButton.addActionListener(e -> {
+            clientIDTextField.setText("");
+            ClientNameTextField.setText("");
+            jobDurationTextField.setText("");
+            jobDeadlineTextField.setText("");
+
+        });
+        submitButton.addActionListener(e -> {
+            JOptionPane.showMessageDialog(panel1,"Submit button has been clicked");
+        });
+
+    }
     public JPanel getPanel() {
         return panel1;
     }
